1c0d2078802496031a54a482c2476735a85678bc
Yuriy Cherkashin <cherkashin.ju@gmail.com>
2021-05-26 00:37:46 +0300

    fix: Formatted with prettier

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 # Training project
+
 ```text
 npm i -g yarn
 ```
@@ -6,8 +7,10 @@ npm i -g yarn
 ## Installed yarn plugins
 
 ### [@yarnpkg/plugin-interactive-tools](https://github.com/yarnpkg/berry/tree/master/packages/plugin-interactive-tools)
+
 Provides the `yarn search` command.  
 It opens a fullscreen terminal interface for search and install packages from the npm registry.
 
 ### [@yarnpkg/plugin-typescript](https://github.com/yarnpkg/berry/tree/master/packages/plugin-typescript)
+
 Automatically adds _@types/ packages_ into dependencies when you add a package that doesn't include its own types
--- index.js
+++ index.js
@@ -1,49 +1,52 @@
-const toDoInput = document.querySelector('.toDoInput')
-const toDoBtn = document.querySelector('.toDoBtn')
+const toDoInput = document.querySelector('.toDoInput');
+const toDoBtn = document.querySelector('.toDoBtn');
 
-const root = document.querySelector('#root')
-let arrayTasks = []
+const root = document.querySelector('#root');
+let arrayTasks = [];
 /* if (toDoInput.value.trim() !== '') {
 
  }*/
 
 const clearTaskList = () => {
-    root.innerHTML = ''
-}
+  root.innerHTML = '';
+};
 let addTaskArr = (event) => {
-    event.preventDefault()
+  event.preventDefault();
 
-    let date = new Date()
-    let month = date.getMonth()
-    let day = date.getDate()
-    if (month === 4) {
-        month = 'may'
-    }
-    clearTaskList()
+  let date = new Date();
+  let month = date.getMonth();
+  let day = date.getDate();
+  if (month === 4) {
+    month = 'may';
+  }
+  clearTaskList();
 
-    let newTask = toDoInput.value
-    toDoInput.value = ''
-    arrayTasks.push({value: newTask, data: {dayDate: '0' + day, monthDate: month}})
-    arrayTasks.forEach(item => {
-        root.append(renderTask(item))
-    })
-    let serialObj = JSON.stringify(arrayTasks)
-    localStorage.setItem("value", serialObj);
-    let returnObj = JSON.parse(localStorage.getItem("value"))
-    returnObj.forEach(item => {
-        arrayTasks.push(item)
-    })
-    console.log('test')
-}
+  let newTask = toDoInput.value;
+  toDoInput.value = '';
+  arrayTasks.push({
+    value: newTask,
+    data: { dayDate: '0' + day, monthDate: month },
+  });
+  arrayTasks.forEach((item) => {
+    root.append(renderTask(item));
+  });
+  let serialObj = JSON.stringify(arrayTasks);
+  localStorage.setItem('value', serialObj);
+  let returnObj = JSON.parse(localStorage.getItem('value'));
+  returnObj.forEach((item) => {
+    arrayTasks.push(item);
+  });
+  console.log('test');
+};
 
 const renderTask = (item) => {
-    const wrapper = document.createElement("div")
-    const task = document.createElement("span")
-    task.classList.add('todoItem')
-    task.innerText = `${item.value}  date: ${item.data.dayDate} ${item.data.monthDate}`
-    wrapper.append(task)
-    return wrapper
-}
+  const wrapper = document.createElement('div');
+  const task = document.createElement('span');
+  task.classList.add('todoItem');
+  task.innerText = `${item.value}  date: ${item.data.dayDate} ${item.data.monthDate}`;
+  wrapper.append(task);
+  return wrapper;
+};
 
 /*const renderAfterLoad = (item) => {
     let serialObj = JSON.stringify(arrayTasks)
